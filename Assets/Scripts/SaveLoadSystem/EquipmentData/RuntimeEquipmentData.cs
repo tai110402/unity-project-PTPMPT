8e06b967ac7186fb87fd051c70a8ac2039ba96a6
vongoctramy <108006461+vongoctramy@users.noreply.github.com>
2023-12-28 13:44:35 +0700

    update runtime equipment data

--- Assets/Scripts/SaveLoadSystem/EquipmentData/RuntimeEquipmentData.cs
+++ Assets/Scripts/SaveLoadSystem/EquipmentData/RuntimeEquipmentData.cs
@@ -7,6 +7,7 @@ public class RuntimeEquipmentData : MonoBehaviour
     private static bool _initializeData = false;
     private static EquipmentData _equipmentData;
     public static EquipmentData EquipmentData { set { _equipmentData = value; } get { return _equipmentData; } }
+    public static bool InitializeData { set { _initializeData = value; } }
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +23,13 @@ public class RuntimeEquipmentData : MonoBehaviour
     {
         _equipmentData = new EquipmentData()
         {
-            ArmorData = new ArmorData() { Armor = new int[] { 100, 200, 300, 400 }, Level = 1, MaxLevel = 4, GoldToUpgradeLevel = new int[] { 30, 40, 50, 60 } },
+            ArmorData = new ArmorData() { Armor = new int[] { 100, 200, 400, 500 }, Level = 1, MaxLevel = 4, GoldToUpgradeLevel = new int[] { 30, 40, 50, 60 } },
             WeaponDataArray = new WeaponData[]
             {
                 new WeaponData { WeaponName = "Shield" ,Damage = new int[] {100, 120, 150, 200}, Level = 1, MaxLevel = 4, GoldToUpgradeLevel = new int[] { 30, 40, 50, 60 } },
                 new WeaponData { WeaponName = "Sword" ,Damage = new int[] {100, 120, 150, 200}, Level = 1, MaxLevel = 4, GoldToUpgradeLevel = new int[] { 30, 40, 50, 60 } },
-                new WeaponData { WeaponName = "Axe" ,Damage = new int[] {130, 150, 170, 250}, Level = 1, MaxLevel = 4, GoldToUpgradeLevel = new int[] { 30, 40, 50, 60 } }
+                //new WeaponData { WeaponName = "Axe" ,Damage = new int[] {300, 400, 500, 600}, Level = 1, MaxLevel = 4, GoldToUpgradeLevel = new int[] { 30, 40, 50, 60 } }
+                new WeaponData { WeaponName = "Axe" ,Damage = new int[] {500, 500, 1000, 2000}, Level = 1, MaxLevel = 4, GoldToUpgradeLevel = new int[] { 30, 40, 50, 60 } } // data test
             }
         };
     }
